a2e847cb60c266422ce77abc561b235ad01eddac
LocalUser <LocalUser@K3-511-21>
2018-12-06 19:41:37 +0500

    Добавьте файлы проекта.

---
+++ ServerWebApplication/AddService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServerWebApplication
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "AddService" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы AddService.svc или AddService.svc.cs в обозревателе решений и начните отладку.
+    public class AddService : IAddService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ServerWebApplication/IAddService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServerWebApplication
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IAddService" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IAddService
+    {
+        [OperationContract]
+        int Add(int a, int b);
+    }
+}
